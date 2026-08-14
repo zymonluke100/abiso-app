0f29e2a8d637ba53eb8ef6308ff2831147c780a3
Luke <judezymon100@gmail.com>
2026-08-14 21:06:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. SUPABASE SETUP
 const supabaseUrl = 'https://xhrzurqxzfaktfqawaru.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhocnp1cnF4emZha3RmcWF3YXJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM0NDIxMDQsImV4cCI6MjAzOTAxODEwNH0.wkdbAsLeYsGiF7k5SlLQzmMXp1DG1x6FQ89RuzhvYQQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhocnp1cnF4emZha3RmcWF3YXJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2OTgyMzgsImV4cCI6MjEwMjI3NDIzOH0.WkdbAsleYsGif7k5slLQzmMXplDGlx6FQ89RuzHvYQQ';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // 2. NAVIGATION (Fixing the Clickable issue)
